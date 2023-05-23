965e49eb064bbfb017a0ed4e1584b11951e34f9c
BrynzevDmitrii <elf_happy@mail.ru>
2023-05-23 18:23:26 +0300

    conain

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -5,10 +5,12 @@ import React from "react";
 import styles from "./style.module.scss";
 import { ContactForm } from "./components/ContactForm/ContactForm";
 import { LearnBlock } from "./components/LearnBlock/LearnBlock";
+import { Corusel } from "./components/Carusel/Carusel";
  
 export const Body = () => {
     return (<section className={styles.body_wrapper}>
         <BannerBlock />
+        <Corusel />
         <ContactForm />
         <TargetAudience />
         <LearnBlock />
