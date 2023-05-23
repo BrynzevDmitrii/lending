1ee19b46ee115fc434d4bcf2e722c7be7a4d852a
BrynzevDmitrii <elf_happy@mail.ru>
2023-05-23 19:10:00 +0300

    carusel

--- src/Body/components/Carusel/Carusel.jsx
+++ src/Body/components/Carusel/Carusel.jsx
@@ -1,38 +1,56 @@
 import Carousel from 'react-bootstrap/Carousel';
+import blue from "./img/blue.jpg"
+
+import styles from "./style.module.scss"
 
 export const  Corusel = () => {
   return (
     <Carousel slide={false}>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
+      <Carousel.Item interval={2000}>
+        <div className={styles.item_first} />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
-        />
+      <Carousel.Item interval={2000}>
+      <div className={styles.item_thecont} />
 
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
 
+      <Carousel.Item interval={2000}>
+      <div className={styles.item_third} />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+      <div className={styles.item_for} />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+      <div className={styles.item_five} />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+      <div className={styles.item_six} />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
